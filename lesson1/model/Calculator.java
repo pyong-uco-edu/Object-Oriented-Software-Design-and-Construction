d60dc9facada9b7f36b1191cf9ee119a8958bd41
philip <philipyong@live.com.my>
2021-08-29 22:05:30 -0500

    CalculatorTest complete

---
+++ lesson1/model/Calculator.java
@@ -0,0 +1,32 @@
+package lesson1.model;
+
+public class Calculator {
+    private double lastN;
+    private double lastN2;
+    private double result;
+
+    public void enter(double n) {
+        lastN2 = lastN;
+        lastN = n;
+    }
+
+    public void add() {
+        result = lastN + lastN2;
+    }
+
+    public void subtract() {
+        result = lastN - lastN2;
+    }
+
+    public void multiply() {
+        result = lastN * lastN2;
+    }
+
+    public void divide() {
+        result = lastN / lastN2;
+    }
+
+    public double getResult() {
+        return result;
+    }
+}
---
+++ lesson1/test/CalculatorTest.java
@@ -0,0 +1,27 @@
+package lesson1.test;
+
+import lesson1.model.Calculator;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator c1 = new Calculator();
+        c1.enter(1);
+        c1.enter(2);
+        c1.add();
+
+        assert c1.getResult() == 3;
+
+        c1.multiply();
+        assert c1.getResult() == 1 * 2;
+        c1.enter(2);
+        c1.enter(3);
+        c1.multiply();
+        assert c1.getResult() == 2 * 3;
+
+        c1.subtract();
+        assert c1.getResult() == 3 - 2;
+
+        c1.divide();
+        assert c1.getResult() == 3.0 / 2;
+    }
+}
